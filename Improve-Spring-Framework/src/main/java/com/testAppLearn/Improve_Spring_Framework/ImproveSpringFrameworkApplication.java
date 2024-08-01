6213a12c66e4bcf31db9503740254703445bc12c
Alialloz <alialloz94450@gmail.com>
2024-08-01 11:55:04 -0400

    Initialisation projet Maven

---
+++ Improve-Spring-Framework/src/main/java/com/testAppLearn/Improve_Spring_Framework/ImproveSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.testAppLearn.Improve_Spring_Framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImproveSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImproveSpringFrameworkApplication.class, args);
+	}
+
+}
