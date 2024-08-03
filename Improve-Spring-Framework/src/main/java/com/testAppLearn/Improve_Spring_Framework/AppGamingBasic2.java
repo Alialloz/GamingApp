1b187ded1fc0a94a49e149f46f76eace73b31b2b
Alialloz <alialloz94450@gmail.com>
2024-08-03 14:46:35 -0400

    Implementation first Spring Bean | Launching Spring Context

--- Improve-Spring-Framework/src/main/java/com/testAppLearn/Improve_Spring_Framework/AppGamingBasic2.java
+++ Improve-Spring-Framework/src/main/java/com/testAppLearn/Improve_Spring_Framework/AppGamingBasic2.java
@@ -2,11 +2,14 @@ package com.testAppLearn.Improve_Spring_Framework;
 
 import com.testAppLearn.Improve_Spring_Framework.game.GameRunner;
 import com.testAppLearn.Improve_Spring_Framework.game.MarioGame;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppGamingBasic2 {
 
     public static void main(String[] args) {
-
+        // 1. Launch Spring Context
+        var context = new AnnotationConfigApplicationContext(HelloWordConfig.class);
+        // 2. Configure things we want spring ton manage - @Configuration
 
     }
 }
---
+++ Improve-Spring-Framework/src/main/java/com/testAppLearn/Improve_Spring_Framework/HelloWordConfig.java
@@ -0,0 +1,7 @@
+package com.testAppLearn.Improve_Spring_Framework;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWordConfig {
+}
