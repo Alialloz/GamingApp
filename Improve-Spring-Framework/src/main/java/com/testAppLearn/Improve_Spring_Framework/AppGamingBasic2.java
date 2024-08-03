acabafdf9128a524ef71f2825c9d6aed9aae7ef6
Alialloz <alialloz94450@gmail.com>
2024-08-03 14:20:00 -0400

    Add AppGamingBasicÃ2 ClassÃ

---
+++ Improve-Spring-Framework/src/main/java/com/testAppLearn/Improve_Spring_Framework/AppGamingBasic2.java
@@ -0,0 +1,12 @@
+package com.testAppLearn.Improve_Spring_Framework;
+
+import com.testAppLearn.Improve_Spring_Framework.game.GameRunner;
+import com.testAppLearn.Improve_Spring_Framework.game.MarioGame;
+
+public class AppGamingBasic2 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
