0aef7513480f962df63034c489e078ab66468b84
Alialloz <alialloz94450@gmail.com>
2024-08-03 06:58:35 -0400

    Implementation class SuperJeu

--- Improve-Spring-Framework/src/main/java/com/testAppLearn/Improve_Spring_Framework/AppGamingBasic.java
+++ Improve-Spring-Framework/src/main/java/com/testAppLearn/Improve_Spring_Framework/AppGamingBasic.java
@@ -2,13 +2,15 @@ package com.testAppLearn.Improve_Spring_Framework;
 
 import com.testAppLearn.Improve_Spring_Framework.game.GameRunner;
 import com.testAppLearn.Improve_Spring_Framework.game.MarioGame;
+import com.testAppLearn.Improve_Spring_Framework.game.SuperJeu;
 
 public class AppGamingBasic {
 
     public static void main(String[] args) {
 
         // Com
-        var marioGame = new MarioGame();
+        // var marioGame = new MarioGame();
+        var superJeu = new SuperJeu();
         var gameRunner = new GameRunner(marioGame);
         gameRunner.run();
 
